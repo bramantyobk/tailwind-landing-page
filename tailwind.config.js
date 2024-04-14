f67997da8d3d5a5477edec41941a9ecad9f1fcfe
bramantyobk <compbramantyobk@gmail.com>
2024-04-14 19:10:53 +0700

    Update Boilerplate

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ["./*.html"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				softBlue: "#5368df",
+				softRed: "#fc5555",
+				grayishBlue: "#9194a1",
+				veryDarkBlue: "#252b46",
+			},
+			fontFamily: {
+				sans: ["Rubik", "sans-serif"],
+			},
+		},
 	},
 	plugins: [],
 };
